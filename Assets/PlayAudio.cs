c2b1bfcb46429f254d316a3fbebc0117ae19bb2f
James Walker <jdwalker08@gmail.com>
2018-02-25 15:34:37 +0000

    dna helixes

---
+++ Assets/PlayAudio.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using VRTK;
+
+public class PlayAudio : MonoBehaviour {
+
+	public UnityEvent triggerEntered;
+
+	private bool hasFired;
+
+
+	public  VRTK.VRTK_InteractableObject interactible;
+
+	private void OnEnable()
+	{
+		interactible.InteractableObjectGrabbed += ObjectGrabbed;
+	}
+	private void OnDisabe()
+	{
+		interactible.InteractableObjectGrabbed -= ObjectGrabbed;
+	}
+
+	private void ObjectGrabbed(object sender, InteractableObjectEventArgs e)
+	{
+		if (!hasFired)
+		{
+			hasFired = true;
+			if (triggerEntered != null)
+			{
+				triggerEntered.Invoke();
+			}
+		}
+	}
+	
+}
